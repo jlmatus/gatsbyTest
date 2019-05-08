902cb3fb77c96b3a9bb56812dd92db0ef4cbed6b
Jose Matus <>
2019-05-08 11:49:09 -0600

    First commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,47 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+import { StaticQuery, graphql } from "gatsby";
+
+const HomePage = () => (
+  <StaticQuery
+    query={graphql`
+      query HomePage {
+        contentfulHomePage {
+          title
+          date
+          content {
+            childMarkdownRemark {
+              html
+            }
+          }
+          image {
+            file {
+              url
+            }
+          }
+        }
+      }
+    `}
+    render={({
+      contentfulHomePage: {
+        title,
+        date,
+        content: {
+          childMarkdownRemark: { html }
+        },
+        image: {
+          file: { url }
+        }
+      }
+    }) => (
+      <>
+        <h1>{title}</h1>
+        <small>Created on {date}</small>
+        <img src={url} />
+        <div dangerouslySetInnerHTML={{ __html: html }} />
+      </>
+    )}
+  />
+);
+
+export default HomePage;
